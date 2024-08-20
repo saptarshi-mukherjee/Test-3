5f0159306aff845cc3103a2864de79c2ef52767c
saptarshi <sapbum1234@gmail.com>
2024-08-20 10:59:36 +0530

    Test Commit

---
+++ src/IntelTest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class IntelTest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.println("Enter first number :-");
+        num1=sc.nextInt();
+        System.out.println("Enter second number :-");
+        num2=sc.nextInt();
+        sum=num1+num2;
+        System.out.println("Sum = "+sum);
+    }
+}
